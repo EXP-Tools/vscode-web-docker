156623b52dc11c15b10693ed6432bf8730ff5218
lyy289065406 <289065406@qq.com>
2020-10-21 00:01:24 +0800

    init

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # vscode-web-docker
-docker 一键部署 vscode-web ：使用浏览器远程开发
 
+> docker 一键部署 vscode-web ：使用浏览器远程开发
+
+------
+
+## 简介
+
+本仓库只是对 [code-server](https://github.com/cdr/code-server) 的 Docker 镜像的简单封装，目的是
 
 
 https://sspai.com/post/60456
-https://github.com/cdr/code-server
 https://hub.docker.com/r/linuxserver/code-server
--- run.sh
+++ run.sh
@@ -2,13 +2,15 @@
 # 运行 vscode-web 服务
 #------------------------------------------------
 # 命令执行示例：
-# ./run.sh -a 123456 -s 123456
+# ./run.sh -a 123456 -s 123456 -u 1000 -g 1000
 #------------------------------------------------
 
 AUTH_PASSWORD="123456"
 SUDO_PASSWORD="123456"
+UID=`id | awk -F '[(=]'  '{print $2}'`
+GID=`id | awk -F '[(=]'  '{print $4}'`
 
-set -- `getopt a:s: "$@"`
+set -- `getopt a:s:u:g: "$@"`
 while [ -n "$1" ]
 do
   case "$1" in
@@ -16,8 +18,12 @@ do
         shift ;;
     -s) SUDO_PASSWORD="$2"
         shift ;;
+    -u) UID="$2"
+        shift ;;
+    -g) GID="$2"
+        shift ;;
   esac
   shift
 done
 
-auth_password=${AUTH_PASSWORD} sudo_password=${SUDO_PASSWORD} docker-compose up -d
+auth_password=${AUTH_PASSWORD} sudo_password=${SUDO_PASSWORD} uid=${UID} gid=${GID} docker-compose up -d
